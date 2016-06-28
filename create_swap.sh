319bc4b97a1c18714aea3540550d45cc92c1709a
Guilherme Souza <souzaux@gmail.com>
2016-06-27 21:55:56 -0300

    modificando o tamanho do arquivo de swap

--- create_swap.sh
+++ create_swap.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
 # size of swapfile in mb
-swapsize=1024
+# swapsize=1024
+swapFile=2048MB.swap
 
 # do we have a swap file?
-grep -q "swapfile" /etc/fstab
+grep -q ${swapFile} /etc/fstab
 
 if [ $? -ne 0 ]; then
 	echo 'No swap file available. Creating...'
-	dd if=/dev/zero of=/var/1024MiB.swap bs=1024k count=1000
+	dd if=/dev/zero of=/var/${swapFile} bs=1024k count=1000
 	#fallocate -l ${swapsize}g /var/1024MiB.swap
-	chmod 600 /var/1024MiB.swap
-	mkswap /var/1024MiB.swap
-	swapon /var/1024MiB.swap
-	echo '/var/1024MiB.swap  none  swap  sw  0 0' >> /etc/fstab
+	chmod 600 /var/${swapFile}
+	mkswap /var/${swapFile}
+	swapon /var/${swapFile}
+	echo '/var/${swapFile}  none  swap  sw  0 0' >> /etc/fstab
 	swapon -a
 	sysctl -w vm.swappiness=10
 	echo vm.swappiness = 10 | tee -a /etc/sysctl.conf
 else
-	echo 'swapfile found. No changes made.'
+	echo '${swapFile} found. No changes made.'
 fi
 
 # show resulting swap file info
