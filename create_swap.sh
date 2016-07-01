b64314fdc67b0a334f4cd1d1d64c686dae68c386
Guilherme Souza <souzaux@gmail.com>
2016-07-01 17:04:34 -0300

    Aumento no tamanho do swapfile e correcao do script

--- create_swap.sh
+++ create_swap.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # size of swapfile in mb
-# swapsize=1024
-swapFile=1024MB.swap
+swapFile=2048MB.swap
 
 # do we have a swap file?
 grep -q ${swapFile} /etc/fstab
@@ -12,16 +11,17 @@ if [ $? -ne 0 ]; then
 	sudo dd if=/dev/zero of=/${swapFile} bs=1024k count=1000
 	#fallocate -l ${swapsize}g /var/1024MiB.swap
 	sudo chmod 600 /${swapFile}
-	sudo mkswap /${swapFile}
+	sudo mkswap -L swap /${swapFile}
 	sudo swapon /${swapFile}
-	echo '/${swapFile}  none  swap  sw  0 0' >> /etc/fstab
-	sudo swapon -a
+	echo /${swapFile}' Swap ativado com sucesso.'
+	echo /${swapFile}'  none  swap  sw  0 0' >> /etc/fstab
+	# sudo swapon -a
 	# Tweak your Swap Settings
 	sudo sysctl -w vm.swappiness=10
 	sudo sysctl vm.vfs_cache_pressure=50
 	echo vm.swappiness = 10 | tee -a /etc/sysctl.conf
 else
-	echo '${swapFile} found. No changes made.'
+	echo /${swapFile}' found. No changes made.'
 fi
 
 # show resulting swap file info
